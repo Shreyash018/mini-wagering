068e7d538741830a9c2da5bc64d8e8241af1b3d5
Shreyash018 <116361255+Shreyash018@users.noreply.github.com>
2024-07-11 10:19:33 +0530

    Update GameDashboardScreen.tsx

--- screens/GameDashboardScreen.tsx
+++ screens/GameDashboardScreen.tsx
@@ -3,7 +3,7 @@ import { View, Button, StyleSheet } from 'react-native';
 import MemoryMatchGame from '../components/MemoryMatchGame';
 
 interface Props {
-    navigation: any; // Assuming you are passing navigation prop to access navigation functions
+    navigation: any; 
     route: { params: { game: { id: string; title: string; description: string; image: string } } };
 }
 
@@ -11,7 +11,7 @@ const GameDashboardScreen: React.FC<Props> = ({ navigation, route }) => {
     const { game } = route.params;
 
     const handleExitGame = () => {
-        navigation.goBack(); // Navigate back to the previous screen
+        navigation.goBack();
     };
 
     return (
